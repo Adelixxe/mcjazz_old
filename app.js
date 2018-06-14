53d3f3102d66cb0290d4e0de9d22352afa783a8e
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2018-06-14 18:18:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const Discord = 'discord.js';
 const bot = new Discord.Client();
+const prefix = "$";
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+bot.on('ready'), () => {
  
+
+
+
+
+
+
+
+
+
+bot.login(process.env.BOT_TOKEN);
