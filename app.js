c9aa9f7b4ba19e4c9e88c1752eb6d6e03997268b
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2018-06-14 18:24:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const prefix = "$";
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
-bot.on('ready'), () => {
+bot.on('ready', () => {
+  console.log('McJazz is ready");   
+    });
  
-
+bot.on('ready', () => {
+  bot.user.setPresence(({ game: { name: " Jazz Lounge", type: 2}}));
+}));
+ 
+ bot.on("message", function (message) {
+  if (message.author.equals(bot.user)) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix)) {
+     message.delete(100)
+  }
 
 
 
