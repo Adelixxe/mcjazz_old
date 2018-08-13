9574934e81c8f111a7fade4804cec99a14bda83d
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2018-08-13 17:01:28 +0200

    test 2 url

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ var cli = new Discord.Client({autoReconnect:true});
 
 bot.commands = new Discord.Collection();
 
-const url = 'https://youtube.com/watch?v=_sI_Ps7JSEk'
+const url = 'https://www.youtube.com/watch?v=eVrYbKBrI7o'
+const url2 = 'https://www.youtube.com/watch?v=MUL5w91dzbo'
+/* https://youtube.com/watch?v=_sI_Ps7JSEk */
 
 bot.on('ready', () => {
     console.log('McJazz is ready');
@@ -46,7 +48,9 @@ bot.on('message', message => {
 
         function music() {
             const stream = message.guild.voiceConnection.playStream(ytdl(url, { filter: 'audioonly' }), streamOptions)
-            .once('end', () => music());
+            .once('end', () =>
+            const stream = message.guild.voiceConnection.playStream(ytdl(url2, { filter: 'audioonly' }), streamOptions)
+            .once('end', () => music ())
         }
     }
     if (message.content.startsWith('$stop')) {
